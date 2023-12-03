41b469dfb879e385216ef7149be2000f69763835
Rithend <rithend21@gmail.com>
2023-12-03 22:13:54 +0530

    Gradients Changed

--- app/page.jsx
+++ app/page.jsx
@@ -6,16 +6,16 @@ const Home = () => {
         <section className="w-full flex-center flex-col">
             <h1 className="head_text text-center">Discover and share
                 <br className="max-md:hidden" />
-                <span className="orange_gradient text-center">
+                <span className="darker_purple_gradient text-center">
                     AI-Powered Prompts
                 </span>
-            </h1>
+            </h1><br />
             <p>
                 Prompter is an open-source AI prompting tool for modern world to discover, create and share creative prompts
             </p>
             <Feed />
         </section>
-        
+
     )
 }
 
